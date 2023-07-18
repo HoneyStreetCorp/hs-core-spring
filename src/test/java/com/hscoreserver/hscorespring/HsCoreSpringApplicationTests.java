b605ac260600973ebbf76ac75edf13289a17c6e1
hoo47 <128340192+hoo47@users.noreply.github.com>
2023-07-18 23:25:33 +0900

    set env

---
+++ src/test/java/com/hscoreserver/hscorespring/HsCoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hscoreserver.hscorespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsCoreSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
