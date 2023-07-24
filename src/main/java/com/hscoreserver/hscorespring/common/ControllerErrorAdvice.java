779163a1ba91b70a80fd83398f95b763fddde9d2
hoo47 <128340192+hoo47@users.noreply.github.com>
2023-07-25 01:23:05 +0900

    Feat: error handler

---
+++ src/main/java/com/hscoreserver/hscorespring/common/ControllerErrorAdvice.java
@@ -0,0 +1,21 @@
+package com.hscoreserver.hscorespring.common;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ControllerErrorAdvice {
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    log.error("Exception occurs: {}", e.getMessage());
+    ErrorResponse errorResponse = ErrorResponse.of(INTERNAL_SERVER_ERROR.toString(), e.getMessage());
+    return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(errorResponse);
+  }
+
+}
---
+++ src/main/java/com/hscoreserver/hscorespring/common/ErrorResponse.java
@@ -0,0 +1,42 @@
+package com.hscoreserver.hscorespring.common;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ErrorResponse {
+  private String errorCode;
+  private String errorMessage;
+
+  public static ErrorResponse of(String errorCode, String errorMessage) {
+    return new ErrorResponse(errorCode, errorMessage);
+  }
+
+  public static ErrorResponse of(String errorCode, BindingResult bindingResult) {
+    return new ErrorResponse(errorCode, createErrorMessage(bindingResult));
+  }
+
+  private static String createErrorMessage(BindingResult bindingResult) {
+    StringBuilder sb = new StringBuilder();
+    boolean isFirst = true;
+    List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+    for (FieldError fieldError : fieldErrors) {
+      if (!isFirst) {
+        sb.append(",");
+      } else {
+        isFirst = false;
+      }
+      sb.append("[");
+      sb.append(fieldError.getField());
+      sb.append("]");
+      sb.append(fieldError.getDefaultMessage());
+    }
+    return sb.toString();
+  }
+}
