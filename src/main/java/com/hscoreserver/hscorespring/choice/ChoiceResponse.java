75bcd2ea6733cffcdd9a03b96139aaf229141cac
hoo47 <128340192+hoo47@users.noreply.github.com>
2023-09-10 13:56:18 +0900

    Refactor: add choices in QuestionResponse

--- src/main/java/com/hscoreserver/hscorespring/choice/ChoiceResponse.java
+++ src/main/java/com/hscoreserver/hscorespring/choice/ChoiceResponse.java
@@ -3,12 +3,11 @@ package com.hscoreserver.hscorespring.choice;
 public record ChoiceResponse(
     Long id,
     int number,
-    String content,
-    Long questionId
+    String content
 ) {
 
 
   public ChoiceResponse(Choice choice) {
-    this(choice.getId(), choice.getNumber(), choice.getContent(), choice.getQuestion().getId());
+    this(choice.getId(), choice.getNumber(), choice.getContent());
   }
 }
--- src/main/java/com/hscoreserver/hscorespring/question/QuestionResponse.java
+++ src/main/java/com/hscoreserver/hscorespring/question/QuestionResponse.java
@@ -1,12 +1,30 @@
 package com.hscoreserver.hscorespring.question;
 
-import lombok.Getter;
+import com.hscoreserver.hscorespring.choice.ChoiceResponse;
+import java.util.List;
 
 
-@Getter
-public class QuestionResponse {
+public record QuestionResponse(
+    Long id,
+    String title,
+    String body,
+    Category category,
+    Long questionSetId,
+    String questionSetName,
+    List<ChoiceResponse> choices
+) {
 
   public QuestionResponse(Question question) {
-
+    this(
+        question.getId(),
+        question.getTitle(),
+        question.getBody(),
+        question.getCategory(),
+        question.getQuestionSet().getId(),
+        question.getQuestionSet().getName(),
+        question.getChoices().stream()
+            .map(ChoiceResponse::new)
+            .toList()
+    );
   }
 }
